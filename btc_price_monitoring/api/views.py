a1b02b2d9bc9b2df088cdbab62d68ed141e9fe94
Philip Sorokin <97840410+philip-sor@users.noreply.github.com>
2022-07-30 20:08:53 +0300

    fixed send_emails
    There was a problem, where only first recipient was getting an email

--- btc_price_monitoring/api/views.py
+++ btc_price_monitoring/api/views.py
@@ -47,25 +47,26 @@ def add_email(request):
 def send_emails(request):
     api_btc_url = 'https://api.coingecko.com/api/v3/coins/bitcoin?localization=en'
     current_btc_price = requests.request('GET', api_btc_url).json()
-	if request.method == "POST":
-		# with open('emails.txt', 'r') as emails:
-		#     emails.seek(0)
-		#     for e in emails.readlines():
-		with open('emails.txt', 'r') as emails:
-			for e in emails.read().split(" "):
-				email = e.rstrip()
-				# print(email)
-				message = Mail(
-					from_email='YOUR EMAIL',
-					to_emails=f'{email}',
-					subject='BTC to UAH price',
-					html_content=f"<strong>{current_btc_price['market_data']['current_price']['uah']}</strong>")
-				sg = SendGridAPIClient("YOUR SENDGRID API KEY")
-				response = sg.send(message)
-				print(email)
-				print(response.status_code)
-				print(response.body)
-				print(response.headers)
-				return JsonResponse({"response": "200",
-									 "description": "Emails were sent"})
+    if request.method == "POST":
+        sg = sendgrid.SendGridAPIClient(api_key="YOUR API KEY")
+        from_email = Email("YOUR EMAIL")
+        subject = "BTC to UAH price"
+        content = Content("text/plain", f"{current_btc_price['market_data']['current_price']['uah']}")
+        mail = Mail(from_email=from_email, to_emails=None, subject=subject, html_content=content)
+        with open('emails.txt', 'r') as emails:
+            for e in emails.read().split(" "):
+                email = e.rstrip()
+		## Creating a list of emails to which the message will be sent. (The recipients of the email will NOT see each others ##
+		## emails address in the TO field.) ##
+                person = Personalization()
+                person.add_to(Email(f"{email}"))
+                mail.add_personalization(person)
+
+            mail_json = mail.get()
+
+            response = sg.client.mail.send.post(request_body=mail_json)
+
+            return JsonResponse({"response": "200",
+                                 "description": "Emails were sent"})
+
 
